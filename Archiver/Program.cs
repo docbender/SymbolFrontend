ed4efcf220dd08954ff81e397dda34b715114e45
docbender <vita.tucek@seznam.cz>
2017-08-30 15:28:44 +0200

    Added project archiver. Archive step7 with SharpZipLib as original project is archive in Step7

---
+++ Archiver/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string logFile = @"D:\Zalohy\Dropbox\Chlmec\backup.log";
+            List<string> log = new List<string>();
+
+
+            string step7Folder = @"D:\PLC\TPCH_S7H";
+            string cimFolder = @"C:\TPCH";
+            string step7ArchiveFolder = @"D:\Zalohy\Dropbox\Chlmec\PLC";
+            string cimArchiveFolder = @"D:\Zalohy\Dropbox\Chlmec\SCADA";
+            //string archivePath = $"{archiveFolder}\\{Path.GetFileName(step7Folder)}_{DateTime.Now.ToString("yyyyMMdd-HHmmss")}.zip";
+            string step7ArchivePath = $"{step7ArchiveFolder}\\{Path.GetFileName(step7Folder)}.zip";
+            string cimArchivePath = $"{cimArchiveFolder}\\{Path.GetFileName(cimFolder)}.zip";
+
+            log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Spoustim zalohu pro {step7Folder} a {cimFolder}");
+            Task<long> t1 = null, t2 = null;
+
+            t1 = Task<long>.Factory.StartNew(() => ZipArchive.ArchiveFolder(step7Folder, step7ArchivePath));
+            t2 = Task<long>.Factory.StartNew(() => ZipArchive.ArchiveFolder(cimFolder, cimArchivePath, new string[] { "\\.Running$", @"\\log\\" }));
+
+            try
+            {
+
+                Task.WaitAll(new Task[] { t1, t2 });
+            }
+            catch (AggregateException ex)
+            {
+                for (int i = 0; i < ex.InnerExceptions.Count; i++)
+                {
+                    Console.WriteLine("!!!---{0}", ex.InnerExceptions[i].ToString());
+                    log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Chyba: {ex.InnerExceptions[i].Message}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("!!!---{0}", ex.ToString());
+                log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Chyba: {ex.Message}");
+            }
+
+            if(t1 != null && t1.Status != TaskStatus.Faulted)
+                log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Provedena zaloha souboru {step7ArchivePath} s velikosti {t1.Result}B");
+            else
+                log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Nastala chyba pri zalohovani do souboru {step7ArchivePath}");
+
+            if (t2!=null && t2.Status != TaskStatus.Faulted)
+                log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Provedena zaloha souboru {cimArchivePath} s velikosti {t2.Result}B");
+            else
+                log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Nastala chyba pri zalohovani do souboru {cimArchivePath}");
+
+            log.Add($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} Zaloha dokoncena");
+
+            try
+            {
+                File.WriteAllLines(logFile, log);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("!!!---Chyba pri zapisu do logu {0}", ex.ToString());
+            }
+
+            Console.WriteLine("Hotovo");
+        }
+    }
+}
---
+++ Archiver/ZipArchive.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Archiver
+{    
+    class ZipArchive
+    {
+        public static long ArchiveFolder(string path, string targetFile)
+        {
+            return ArchiveFolder(path, targetFile, null);
+        }
+        public static long ArchiveFolder(string path, string targetFile, string[] excluedeFileMask)
+        {
+            if (!Directory.Exists(path))
+            {
+                throw new DirectoryNotFoundException();
+            }
+
+            ZipOutputStream zip = new ZipOutputStream(File.Create(targetFile));
+            zip.SetLevel(9);
+            ZipFolder(path, Path.GetFileName(path), path, zip, excluedeFileMask);
+            zip.Finish();
+            var size = zip.Length;
+            zip.Close();
+
+            return size;
+        }
+
+        public static void ZipFolder(string RootFolderPath, string RootFolderName, string CurrentFolderPath, ZipOutputStream zStream, string[] excluedeFileMask)
+        {
+            string[] SubFolders = Directory.GetDirectories(CurrentFolderPath);
+
+            foreach (string Folder in SubFolders)
+                ZipFolder(RootFolderPath, RootFolderName, Folder, zStream, excluedeFileMask);
+            
+            string relativePath = CurrentFolderPath.Substring(RootFolderPath.Length - RootFolderName.Length - 1) + "/";
+
+            if (relativePath.Length > 1)
+            {
+                ZipEntry dirEntry;
+
+                dirEntry = new ZipEntry(relativePath);
+                dirEntry.DateTime = DateTime.Now;
+            }
+
+            foreach (string file in Directory.GetFiles(CurrentFolderPath))
+            {                
+                if(excluedeFileMask != null)
+                {
+                    bool match = false;
+                    foreach (var m in excluedeFileMask)
+                    {
+                        if(Regex.IsMatch(file, m))
+                        {
+                            match = true;
+                            break;
+                        }
+                    }
+
+                    if (match)
+                        continue;
+                }
+
+                AddFileToZip(zStream, relativePath, file);
+            }
+        }
+
+        private static void AddFileToZip(ZipOutputStream zStream, string relativePath, string file)
+        {
+            byte[] buffer = new byte[4096];
+            string fileRelativePath = (relativePath.Length > 1 ? relativePath : string.Empty) + Path.GetFileName(file);
+            ZipEntry entry = new ZipEntry(fileRelativePath);
+
+            entry.DateTime = DateTime.Now;
+            zStream.PutNextEntry(entry);
+
+            using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                int sourceBytes;
+
+                do
+                {
+                    sourceBytes = fs.Read(buffer, 0, buffer.Length);
+                    zStream.Write(buffer, 0, sourceBytes);
+                } while (sourceBytes > 0);
+            }
+        }
+    }
+}
